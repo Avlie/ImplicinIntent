ee38eb4be49c606b1037da48cf6fe701eefda33f
Avlie <antipovicana@gmail.com>
2025-02-11 11:33:29 +0100

    Initial commit

---
+++ app/src/main/java/com/example/implicinintent/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.implicinintent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button button = findViewById(R.id.button);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        button.setOnClickListener(v -> openwebpage("https://www.google.com"));
+
+    }
+    private void openwebpage(String url){
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        try {
+            startActivity(intent);
+            }
+        catch (Exception e){
+            Log.e("ERROR", "Brak aplikacji do obsluzenia",e );
+        }
+    }
+}
