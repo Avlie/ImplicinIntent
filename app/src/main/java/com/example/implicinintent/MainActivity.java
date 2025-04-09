ce8feb0876d91d22437e64f7d89d2750d2f21474
Avlie <antipovicana@gmail.com>
2025-04-09 08:08:16 +0200

    button

--- app/src/main/java/com/example/implicinintent/MainActivity.java
+++ app/src/main/java/com/example/implicinintent/MainActivity.java
@@ -12,6 +12,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -28,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
         Button button1 = findViewById(R.id.button1);
         Button button2 = findViewById(R.id.button2);
         button.setOnClickListener(v -> openwebpage("https://www.google.com"));
+        button1.setOnClickListener(v -> number("123456789"));
+        button2.setOnClickListener(view -> sendmail("d", "ddd", "dddd"));
+
+
 
     }
     private void openwebpage(String url){
@@ -39,5 +45,29 @@ public class MainActivity extends AppCompatActivity {
         catch (Exception e){
             Log.e("ERROR", "Brak aplikacji do obsluzenia",e );
         }
+    }
+
+    private void number(String number){
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + number));
+        try {
+            startActivity(intent);
+            }
+        catch (Exception e){
+            Log.e("ERROR", "Brak aplikacji do obsluzenia",e );
+        }
+    }
+    private void sendmail(String addres, String subject, String body){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{addres});
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, body);
+        try {
+            startActivity(intent);
+        } catch (Exception e) {
+            Log.e("ERROR", "Brak aplikacji do obsluzenia", e);
+        }
+
     }
 }
